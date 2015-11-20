e996fdb1f0f820cbd36a89cbb9d65af26179f4cc
Marco Semiao <marco.semiao@gmail.com>
2015-11-20 11:55:17 +0100

    implementation de sqlOperation pour les TU

---
+++ core/log4jdbc-driver/src/test/java/fr/ms/log4jdbc/test/sqloperation/SqlMessage.java
@@ -0,0 +1,37 @@
+package fr.ms.log4jdbc.test.sqloperation;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class SqlMessage {
+
+    private final List<SqlOperationMessage> messages = new ArrayList<SqlOperationMessage>();
+
+    private final static SqlMessage instance = new SqlMessage();
+
+    public final static SqlMessage getInstance() {
+	return instance;
+    }
+
+    public List<SqlOperationMessage> getSqlMessages() {
+	return getSqlMessages(true);
+    }
+
+    public List<SqlOperationMessage> getSqlMessages(final boolean erase) {
+	final List<SqlOperationMessage> list = new ArrayList<SqlOperationMessage>(messages);
+	if (erase) {
+	    clear();
+	}
+	return Collections.unmodifiableList(list);
+
+    }
+
+    public void clear() {
+	messages.clear();
+    }
+
+    void addMessage(final SqlOperationMessage message) {
+	messages.add(message);
+    }
+}
