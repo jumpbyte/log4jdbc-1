f50dad34662a598e772bc70d506bbff6aefe4326
Marco Semiao <marco.semiao@gmail.com>
2015-11-25 15:12:20 +0100

    correction

--- core/log4jdbc-driver/src/main/java/fr/ms/log4jdbc/proxy/operation/PreparedStatementOperation.java
+++ core/log4jdbc-driver/src/main/java/fr/ms/log4jdbc/proxy/operation/PreparedStatementOperation.java
@@ -75,7 +75,7 @@ public class PreparedStatementOperation extends StatementOperation {
 	    return;
 	}
 
-	buildSqlOperation();
+	super.buildSqlOperation();
     }
 
     private QueryImpl createWrapperQuery(final QueryImpl query) {
