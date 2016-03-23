21b033226ce0c63dff8e13af64b173d702bb5fe1
Marco Semiao <marco.semiao@gmail.com>
2016-03-23 11:49:13 +0100

    add licence

--- core/log4jdbc-driver/src/main/java/fr/ms/log4jdbc/proxy/handler/Log4JdbcInvocationHandler.java
+++ core/log4jdbc-driver/src/main/java/fr/ms/log4jdbc/proxy/handler/Log4JdbcInvocationHandler.java
@@ -1,3 +1,20 @@
+/*
+ * This file is part of Log4Jdbc.
+ *
+ * Log4Jdbc is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Log4Jdbc is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Log4Jdbc.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
 package fr.ms.log4jdbc.proxy.handler;
 
 import java.lang.reflect.Method;
@@ -12,6 +29,14 @@ import fr.ms.log4jdbc.SqlOperationLogger;
 import fr.ms.log4jdbc.sql.FormatQuery;
 import fr.ms.log4jdbc.sql.FormatQueryFactory;
 
+/**
+ *
+ * @see <a href="http://marcosemiao4j.wordpress.com">Marco4J</a>
+ *
+ *
+ * @author Marco Semiao
+ *
+ */
 public class Log4JdbcInvocationHandler extends ProxyOperationInvocationHandler {
 
     private final SqlOperationLogger[] logs;
--- core/log4jdbc-driver/src/main/java/fr/ms/log4jdbc/proxy/xa/operation/factory/XAResourceOperationFactory.java
+++ core/log4jdbc-driver/src/main/java/fr/ms/log4jdbc/proxy/xa/operation/factory/XAResourceOperationFactory.java
@@ -1,3 +1,20 @@
+/*
+ * This file is part of Log4Jdbc.
+ *
+ * Log4Jdbc is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Log4Jdbc is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Log4Jdbc.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
 package fr.ms.log4jdbc.proxy.xa.operation.factory;
 
 import java.lang.reflect.Method;
@@ -8,6 +25,14 @@ import fr.ms.lang.reflect.TimeInvocation;
 import fr.ms.log4jdbc.context.xa.Log4JdbcContextXA;
 import fr.ms.log4jdbc.proxy.xa.operation.XAResourceOperation;
 
+/**
+ *
+ * @see <a href="http://marcosemiao4j.wordpress.com">Marco4J</a>
+ *
+ *
+ * @author Marco Semiao
+ *
+ */
 public class XAResourceOperationFactory implements ProxyOperationFactory {
 
     private final Log4JdbcContextXA log4JdbcContext;
